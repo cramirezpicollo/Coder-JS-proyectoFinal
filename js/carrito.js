4708a49c1a78cb45670431d446792470b7268726
cramirezpicollo <cramirezpicollo@gmail.com>
2023-11-16 18:54:48 -0300

    entrega final

--- js/carrito.js
+++ js/carrito.js
@@ -59,14 +59,6 @@ const pintarCarrito = () => {
             eliminarProducto(product.id);
         })
 
-
-        //let eliminar = document.createElement("span");
-       // eliminar.innerText = "❌";
-        //eliminar.className = "delete-product";
-        //arritoContent.append(eliminar);
-
-        //eliminar.addEventListener("click", eliminarProducto)
-
     });
 
     const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
